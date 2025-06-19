2e69a6ac1e64c748f0bd11d674403d65a5fc6688
leif <andersonmolinagonzales0@gmail.com>
2025-06-19 15:57:17 -0400

    docs(detail): Updated documentation for detail use cases
    - Added
     documentation for getDetail use case
    
    - Removed outdated documentation from listDetails use case

--- src/modules/detail/application/getDetail.ts
+++ src/modules/detail/application/getDetail.ts
@@ -1,8 +1,12 @@
 import { UUID } from "../../shared/domain/UUID.js";
+import { PrimitiveDetail } from "../domain/detail.js";
 import { DetailNotFoundError } from "../domain/detailNotFoundError.js";
 import { DetailRepository } from "../domain/detailRepository.js";
 import { DetailTitle } from "../domain/detailTitle.js";
 
+/**
+ * Class to get a detail by its ID or title.
+ */
 export class GetDetail {
   private readonly detailRepository: DetailRepository;
 
@@ -10,29 +14,46 @@ export class GetDetail {
     this.detailRepository = params.detailRepository;
   }
 
+  /**
+   * Gets a detail by its ID.
+   * @param detailId ID of the detail to get.
+   * @returns The detail in primitive format.
+   * @throws {DetailNotFoundError} If the detail is not found.
+   */
   private async getDetailById(detailId: UUID) {
     const detail = await this.detailRepository.find({ detailId });
     if (!detail) throw new DetailNotFoundError({ detailId });
     return detail.toPrimitives();
   }
 
+  /**
+   * Gets a detail by its title.
+   * @param detailTitle Title of the detail to get.
+   * @returns The detail in primitive format.
+   * @throws {DetailNotFoundError} If the detail is not found.
+   */
   private async getDetailByTitle(detailTitle: DetailTitle) {
     const detail = await this.detailRepository.find({ detailTitle });
     if (!detail) throw new DetailNotFoundError({ detailTitle });
     return detail.toPrimitives();
   }
 
-  async run(params: { detailId: UUID }): Promise<{
-    detailId: string;
-    detailTitle: string;
-  }>;
+  /**
+   * Executes the search for a detail by ID.
+   * @param params Object with the ID of the detail to search for.
+   * @returns The detail in primitive format.
+   */
+  async run(params: { detailId: UUID }): Promise<PrimitiveDetail>;
+  /**
+   * Executes the search for a detail by title.
+   * @param params Object with the title of the detail to search for.
+   * @returns The detail in primitive format.
+   */
+  async run(params: { detailTitle: DetailTitle }): Promise<PrimitiveDetail>;
 
-  async run(params: { detailTitle: DetailTitle }): Promise<{
-    detailId: string;
-    detailTitle: string;
-  }>;
-
-  async run(params: { detailId: UUID } | { detailTitle: DetailTitle }) {
+  async run(
+    params: { detailId: UUID } | { detailTitle: DetailTitle }
+  ): Promise<PrimitiveDetail> {
     const isDetailId = "detailId" in params;
     if (isDetailId) return this.getDetailById(params.detailId);
     return this.getDetailByTitle(params.detailTitle);
--- src/modules/detail/application/listDetails.ts
+++ src/modules/detail/application/listDetails.ts
@@ -15,9 +15,6 @@ export class ListDetails {
    * @param params.limit - The maximum number of details to return, must be a positive integer.
    * @param params.offset - The number of details to skip before starting to collect the result set, must be a non-negative integer.
    * @returns An array of details, each represented as a primitive object.
-   *
-   * @throws {PositiveIntegerError} If the limit is not a positive integer.
-   * @throws {NonNegativeError} If the offset is not a non-negative integer.
    */
   async run(params: { limit: PositiveInteger; offset: NonNegativeInteger }) {
     const { limit, offset } = params;
